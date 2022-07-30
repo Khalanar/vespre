bbcb9275ac41cb2bc2bf74755651bfbf2a532c40
Khalanar <pablo.xez@gmail.com>
2022-07-30 17:51:03 +0000

    add checkout via stripe incl wb, templates and order creation

--- checkout/views.py
+++ checkout/views.py
@@ -9,7 +9,7 @@ from cart.contexts import cart_contents
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 
-
+import stripe
 import json
 
 
@@ -17,12 +17,12 @@ import json
 def cache_checkout_data(request):
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
-        # stripe.api_key = settings.STRIPE_SECRET_KEY
-        # stripe.PaymentIntent.modify(pid, metadata={
-        #     'cart': json.dumps(request.session.get('cart', {})),
-        #     'save_info': request.POST.get('save_info'),
-        #     'username': request.user,
-        # })
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
         return HttpResponse(status=200)
     except Exception as e:
         messages.error(request, 'Sorry, your payment cannot be \
@@ -31,10 +31,12 @@ def cache_checkout_data(request):
 
 
 def checkout(request):
-    # stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    # stripe_secret_key = settings.STRIPE_SECRET_KEY
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
+        print("Form POST")
+
         cart = request.session.get('cart', {})
 
         form_data = {
@@ -51,6 +53,7 @@ def checkout(request):
 
         order_form = OrderForm(form_data)
         if order_form.is_valid():
+            print("form is valid")
             order = order_form.save(commit=False)
             pid = request.POST.get('client_secret').split('_secret')[0]
             order.stripe_pid = pid
@@ -87,6 +90,8 @@ def checkout(request):
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
+            print("form is not valid")
+
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
     else:
@@ -97,12 +102,14 @@ def checkout(request):
 
         current_cart = cart_contents(request)
         total = current_cart['total']
-        # stripe_total = round(total * 100)
-        # stripe.api_key = stripe_secret_key
-        # intent = stripe.PaymentIntent.create(
-        #     amount=stripe_total,
-        #     currency=settings.STRIPE_CURRENCY,
-        # )
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        # print(intent)
 
         # Attempt to prefill the form with any info the user maintains in their profile
     #     if request.user.is_authenticated:
@@ -125,15 +132,15 @@ def checkout(request):
     #         order_form = OrderForm()
         order_form = OrderForm()
 
-    # if not stripe_public_key:
-    #     messages.warning(request, 'Stripe public key is missing. \
-    #         Did you forget to set it in your environment?')
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'spk',
-        'client_secret': 'cs',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
@@ -147,25 +154,25 @@ def checkout_success(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
+        # profile = UserProfile.objects.get(user=request.user)
         # Attach the user's profile to the order
-        order.user_profile = profile
+        # order.user_profile = profile
         order.save()
 
         # Save the user's info
-        if save_info:
-            profile_data = {
-                'default_phone_number': order.phone_number,
-                'default_country': order.country,
-                'default_postcode': order.postcode,
-                'default_town_or_city': order.town_or_city,
-                'default_street_address1': order.street_address1,
-                'default_street_address2': order.street_address2,
-                'default_county': order.county,
-            }
-            user_profile_form = UserProfileForm(profile_data, instance=profile)
-            if user_profile_form.is_valid():
-                user_profile_form.save()
+        # if save_info:
+        #     profile_data = {
+        #         'default_phone_number': order.phone_number,
+        #         'default_country': order.country,
+        #         'default_postcode': order.postcode,
+        #         'default_town_or_city': order.town_or_city,
+        #         'default_street_address1': order.street_address1,
+        #         'default_street_address2': order.street_address2,
+        #         'default_county': order.county,
+        #     }
+        #     user_profile_form = UserProfileForm(profile_data, instance=profile)
+        #     if user_profile_form.is_valid():
+        #         user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
--- vespre/settings.py
+++ vespre/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -180,5 +183,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'dsdsds')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'dsdsds')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
