02dc6828893caeffc3be4acdfc216107f9ad7fcd
Khalanar <pablo.xez@gmail.com>
2022-07-30 10:27:59 +0000

    add toast templates and messages for cart events

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.contrib import messages
 from products.models import Product
 
 
@@ -11,6 +12,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add product to cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
@@ -28,11 +30,16 @@ def add_to_cart(request, item_id):
                 cart[item_id]['items_by_size'][size] = quantity
         else:
             cart[item_id] = {'items_by_size': {size: quantity}}
+
+        messages.success(request, f'Added {quantity} x { product } size { size.upper() } to your cart!')
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
         else:
             cart[item_id] = quantity
+        
+        messages.success(request, f'Added {quantity} x { product } to your cart!')
+            
 
     request.session['cart'] = cart
 
@@ -58,6 +65,7 @@ def adjust_cart(request, item_id):
             del cart[item_id]['items_by_size'][size]
             if not cart[item_id]['items_by_size']:
                 cart.pop(item_id)
+
     else:
         if quantity > 0:
             cart[item_id] = quantity
@@ -65,6 +73,8 @@ def adjust_cart(request, item_id):
             cart.pop(item_id)
 
     request.session['cart'] = cart
+    messages.success(request, f'Cart successfully modified!')
+
     return redirect(reverse('view_cart'))
 
 
@@ -85,8 +95,12 @@ def remove_from_cart(request, item_id):
         else:
             cart.pop(item_id)
 
+        messages.success(request, f'{ product } successfully removed from your cart.')
+
         request.session['cart'] = cart
         return HttpResponse(status=200)
+    
 
     except Exception as e:
+        messages.error(request, f'Error removing items: { e }')
         return HttpResponse(status=500)
