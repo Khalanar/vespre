280eda475db6ec8a036a1284297cdb2369810a0a
Khalanar <pablo.xez@gmail.com>
2022-07-31 08:26:37 +0000

    add templates, urls and views for basic profile implementation

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .models import UserProfile
+
+from checkout.models import Order
+
+
+@login_required
+def profile(request):
+    """ Display the user's profile. """
+    template = 'profiles/profile.html'
+
+    context = {}
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
