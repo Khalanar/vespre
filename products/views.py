91fd572804c98710d046edb7b0459af6a5a13bd7
Khalanar <pablo.xez@gmail.com>
2022-08-01 18:23:35 +0000

    add functionality for superusers to add products

--- products/views.py
+++ products/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
 from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product
-from django.conf import settings
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -14,8 +17,6 @@ def all_products(request):
     query = None
     sort = None
     direction = None
-    currency_symbol = settings.CURRENCY_SYMBOL
-    currency = settings.CURRENCY
     max_rating = [0, 1, 2, 3, 4]
 
     if request.GET:
@@ -60,9 +61,6 @@ def all_products(request):
 
     context = {
         'products': products,
-        'currency_symbol': currency_symbol,
-        'currency': currency,
-        'max_rating':  max_rating,
         'search_term': query,
         'current_sorting': current_sorting
     }
@@ -73,15 +71,77 @@ def all_products(request):
 def product_detail(request, product_id):
     """ View that returns one product by its ID """
     product = get_object_or_404(Product, pk=product_id)
-    currency_symbol = settings.CURRENCY_SYMBOL
-    currency = settings.CURRENCY
     max_rating = [0, 1, 2, 3, 4]
 
     context = {
         'product': product,
-        'currency_symbol': currency_symbol,
-        'currency': currency,
-        'max_rating': max_rating,
     }
 
     return render(request, 'products/product_detail.html', context)
+
+@login_required
+def add_product(request):
+    """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
