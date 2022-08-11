6b6a3f9a6800419d7d5717593a2e074c4a633bd0
Khalanar <pablo.xez@gmail.com>
2022-08-11 09:24:33 +0000

    add discount app and models

---
+++ discounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class DiscountType(models.Model):
+    """
+    sad
+    """
+
+class Discount(models.Model):
+    """
+    sdad
+    """
+
