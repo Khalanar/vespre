f80a07e844ea33fc07cafb3b8f229ea3d710a5cf
Khalanar <pablo.xez@gmail.com>
2022-08-13 07:46:47 +0000

    add styling to discount forms in checkout

--- discounts/forms.py
+++ discounts/forms.py
@@ -26,3 +26,4 @@ class ApplyDiscountForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-primary'
+            field.label = False
