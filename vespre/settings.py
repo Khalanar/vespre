c3d6a05f2abb016372d93615189694d3ec8816b7
Khalanar <pablo.xez@gmail.com>
2022-08-15 16:41:49 +0000

    add server we-eu61 to allowed hosts

--- vespre/settings.py
+++ vespre/settings.py
@@ -32,10 +32,12 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'vespre.herokuapp.com',
                  '8000-khalanar-vespre-wbfchs5rflz.ws-eu59.gitpod.io',
-                 '8000-khalanar-vespre-wbfchs5rflz.ws-eu60.gitpod.io']
+                 '8000-khalanar-vespre-wbfchs5rflz.ws-eu60.gitpod.io',
+                 '8000-khalanar-vespre-wbfchs5rflz.ws-eu61.gitpod.io'],
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-khalanar-vespre-wbfchs5rflz.ws-eu59.gitpod.io',
                         'https://8000-khalanar-vespre-wbfchs5rflz.ws-eu60.gitpod.io',
+                        'https://8000-khalanar-vespre-wbfchs5rflz.ws-eu61.gitpod.io',
                         'https://vespre.herokuapp.com']
 
 # Application definition
