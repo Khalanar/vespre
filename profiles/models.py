1d8f76816592af69b17dc2d804c6325a09c5a5ce
Khalanar <pablo.xez@gmail.com>
2022-08-15 16:35:29 +0000

    add docstrings in profile models

--- profiles/models.py
+++ profiles/models.py
@@ -20,7 +20,9 @@ class UserProfile(models.Model):
     default_county = models.CharField(max_length=80, null=True, blank=True)
     
     def __str__(self):
-
+        """
+        return profile username as a string
+        """
         return self.user.username
 
 
